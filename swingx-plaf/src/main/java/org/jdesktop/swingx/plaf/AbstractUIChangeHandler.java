5cc0d99a8ec0a68198bd429257cb58af2311023b
Christian Bering <bering@softec-ag.de>
2020-01-03 11:54:37 +0100

    made installed map usage synchronized
    * is statically used in inheriting classes; this led to an invalid
      map in certain multi-threaded contexts

--- swingx-plaf/src/main/java/org/jdesktop/swingx/plaf/AbstractUIChangeHandler.java
+++ swingx-plaf/src/main/java/org/jdesktop/swingx/plaf/AbstractUIChangeHandler.java
@@ -6,26 +6,29 @@ import java.util.WeakHashMap;
 
 import javax.swing.JComponent;
 
-@SuppressWarnings("nls")
 public abstract class AbstractUIChangeHandler implements PropertyChangeListener {
-	//prevent double installation.
+	// prevent double installation.
 	private final Map<JComponent, Boolean> installed = new WeakHashMap<JComponent, Boolean>();
-	
-	public void install(JComponent c){
-		if(isInstalled(c)){
-			return;
+
+	public void install(JComponent c) {
+		synchronized (installed) {
+			if (!isInstalled(c)) {
+				c.addPropertyChangeListener("UI", this);
+				installed.put(c, Boolean.TRUE);
+			}
 		}
-		
-		c.addPropertyChangeListener("UI", this);
-		installed.put(c, Boolean.FALSE);
-	}
-	
-	public boolean isInstalled(JComponent c) {
-		return installed.containsKey(c);
 	}
 
-    public void uninstall(JComponent c){
-		c.removePropertyChangeListener("UI", this);
-		installed.remove(c);
+	public boolean isInstalled(JComponent c) {
+		synchronized (installed) {
+			return installed.containsKey(c);
+		}
+	}
+
+	public void uninstall(JComponent c) {
+		synchronized (installed) {
+			c.removePropertyChangeListener("UI", this);
+			installed.remove(c);
+		}
 	}
 }
